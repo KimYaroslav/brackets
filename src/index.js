d61e99b64cbe9e5e8528a83e3bf27b96062314d8
KimYaroslav <Korean_mafia_01@mail.ru>
2023-02-16 14:46:27 +0600

    feat: add task solution in js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let objConf = Object.fromEntries(bracketsConfig);
+  let objKey = [];
+  let arr = [];
+
+  for (let key in objConf) {
+    objKey.push(key);
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    let elem = str[i];
+
+    if (arr.length === 0) {
+
+      if (objKey.includes(elem)) {
+        arr.push(elem);
+      } else return false;
+
+    } else {
+
+      let previous = arr[arr.length - 1];
+
+      if (objKey.includes(elem)) {
+        if ((objConf[elem] === elem) && (objConf[previous] === elem)) {
+          arr.pop();
+        } else {
+          arr.push(elem);
+        }
+      } else {
+        if (objConf[previous] === elem) {
+          arr.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return Boolean(arr.length === 0);
 }
